a3d351e264c2fc52ee7db2cc7d541c2e38eaa1d2
nick_mattis <nickm970@gmail.com>
2020-06-16 20:38:07 -0400

    remove some dependencies and use IPIFY for more stable pub ip address, update outdated dependecies

--- pub_ip_check.py
+++ pub_ip_check.py
@@ -1,15 +1,13 @@
+import json
 import logging
-import re
 import smtplib
 
 import requests
-from bs4 import BeautifulSoup
 
-SEARCH_URL = 'https://www.google.com/search?q=what%27s+my+ip'
-IP_REGEX = "^(?:[0-9]{1,3}.){3}[0-9]{1,3}$"
+IPIFY_PUB_IP_URL = 'https://api6.ipify.org?format=json'
 FILE_NAME = 'ip.txt'
 OUTGOING = 'smtp.gmail.com'
-PORT = 465 
+PORT = 465
 USER_NAME = '<gmail user>'
 PASSWORD = '<gmail password>'
 EMAIL_RECIPIENT = '<email recipient>'
@@ -31,40 +29,32 @@ def main():
         datefmt='%m/%d/%Y %I:%M:%S %p'
     )
     try:
-        search_result = requests.get(SEARCH_URL)
-        if search_result.status_code == requests.codes.ok:
-            html = BeautifulSoup(search_result.content, 'html.parser')
-            ip_check = re.compile(IP_REGEX)
-            for div in html.body.find('div', {'id': 'search'}):
-                children = div.findChildren('div', recursive=True)
-                for child in children:
-                    if ip_check.fullmatch(child.text):
-                        try:
-                            with open(FILE_NAME, 'r') as file:
-                                last_ip = file.readlines()
-                            if not last_ip[0].strip() == child.text.strip():
-                                # write out new ip, send update email
-                                last_ip[0] = child.text.strip()
-                                with open(FILE_NAME, 'w') as file:
-                                    file.writelines(child.text.strip())
+        pub_ip_address = requests.get(IPIFY_PUB_IP_URL)
+        if pub_ip_address.status_code == requests.codes.ok:
+            ip_address = json.loads(pub_ip_address.text)['ip']
+            try:
+                with open(FILE_NAME, 'r') as file:
+                    last_ip = file.readlines()
+                if not last_ip[0].strip() == ip_address:
+                    # write out new ip, send update email
+                    with open(FILE_NAME, 'w') as file:
+                        file.writelines(ip_address)
 
-                                send_email(child.text.strip())
-                                logging.info(f'New IP is {child.text.strip()}')
-                            else:
-                                logging.info('IP has not changed.')
-                        except FileNotFoundError:
-                            # no last ip, must be first run, write to file, send email
-                            file = open(FILE_NAME, 'w+')
-                            file.write(child.text.strip())
+                    send_email(ip_address)
+                    logging.info(f'New IP is {ip_address}')
+                else:
+                    logging.info('IP has not changed.')
+            except FileNotFoundError:
+                # no last ip, must be first run, write to file, send email
+                with open(FILE_NAME, 'w+') as file:
+                    file.write(ip_address)
 
-                            send_email(child.text.strip())
-                            logging.info(f'New IP is {child.text.strip()}')
-                        finally:
-                            file.close()
+                send_email(ip_address)
+                logging.info(f'New IP is {ip_address}')
         else:
-            logging.info(f'Request responded with {search_result.status_code} status code')
+            logging.error(f'Request responded with {search_result.status_code} status code')
     except requests.exceptions.RequestException as e:
-        logging.info(f'Something went really wrong. {e}')
+        logging.error(f'Something went really wrong. {e}')
 
 
 if __name__ == '__main__':
